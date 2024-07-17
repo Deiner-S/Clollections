243462edc37ca90304f98c560441aaf0d51c95f8
Deiner Rodrigues de Souza <deinersouza.tmsi@gmail.com>
2024-07-17 15:04:13 -0300

    exemplos Set incompleto

---
+++ src/Exemplos/ExemploHashSet.java
@@ -0,0 +1,30 @@
+package Exemplos;
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class ExemploHashSet {
+
+	public static void main(String[] args) {
+		ExemploHashSet();
+	}
+	
+	public static void ExemploHashSet(){
+		
+		Set<String> lista = new HashSet<>();
+		
+		lista.add("Item-1");
+		lista.add("Item-2");
+		//lista.add("Item-1");
+		lista.add("Item-3");
+		//lista.add("Item-1");
+		lista.add("Item-4");
+		
+		System.out.println(lista);
+		System.out.println("");
+		
+		
+		
+	}
+
+}
